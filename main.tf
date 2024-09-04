cdefc1631b6da3e2d2c52b854b91ab3a23d4382d
Per Hartveit <per.hartveit@knowit.no>
2024-09-04 16:12:23 +0200

    adding azure container registry to avoid using docker's, since docker's is rate limited

--- main.tf
+++ main.tf
@@ -53,6 +53,14 @@ resource "azurerm_linux_web_app" "webapp" {
   }
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = "per-dojo-container-registry"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
 resource "azurerm_container_group" "ctg" {
   name                = "notesapp"
   location            = azurerm_resource_group.rg.location
