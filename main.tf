f594d156c8435fbd4d25b850bf13839defdb35c3
Per Hartveit <per.hartveit@knowit.no>
2024-09-04 17:13:14 +0200

    seperated terraform instructions. Irrelevant but working instruction in own file.

--- main.tf
+++ main.tf
@@ -22,37 +22,6 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-
-
-# Create the Linux App Service Plan
-resource "azurerm_service_plan" "appserviceplan" {
-  name                = "notesapp-webapp-asp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "F1"
-  tags = {
-    environment = "dev"
-  }
-}
-
-
-# Create the web app, pass in the App Service Plan ID
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "notesapp-webapp-${random_integer.ri.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.appserviceplan.id
-  https_only          = true
-  site_config {
-    minimum_tls_version = "1.2"
-    always_on           = false
-  }
-  tags = {
-    environment = "dev"
-  }
-}
-
 // doesn't use docker hub anymore, instead pulls from the images in the resource group's private image registry
 
 resource "azurerm_container_registry" "acr" {
---
+++ standalone-linux-webapp-howto.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+provider "azurerm" {
+  features {}
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "per-dojo"
+  location = "West Europe"
+}
+
+
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "notesapp-webapp-asp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1"
+  tags = {
+    environment = "dev"
+  }
+}
+
+
+# Create the web app, pass in the App Service Plan ID
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "notesapp-webapp-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.appserviceplan.id
+  https_only          = true
+  site_config {
+    minimum_tls_version = "1.2"
+    always_on           = false
+  }
+  tags = {
+    environment = "dev"
+  }
+}
